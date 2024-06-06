60120129cdf6e0e9cfea10694328bbdc87df3eed
koni24 <konisha.24.kar@gmail.com>
2024-06-07 00:13:25 +0530

    navbar and banner created

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Home from '@/modules/home/home'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default Page
---
+++ src/modules/home/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "@/shared/widgets/header/header"
+import Banner from './features/banner'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+    </div>
+  )
+}
+
+export default Home
